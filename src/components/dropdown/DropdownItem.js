4a654975d4c17cfc68abe4232c2c4589855334a6
Daarrik <darrikwh@gmail.com>
2022-01-25 15:50:50 -0800

    token testing

--- src/components/dropdown/DropdownItem.js
+++ src/components/dropdown/DropdownItem.js
@@ -1,13 +1,13 @@
-import React from 'react';
+// import React from 'react';
 
-const DropdownItem = (props) => {
-  return (
-    <li className='dropdown-item'>
-      <a href={props.url}>
-        {props.title}
-      </a>
-    </li>
-  );
-}
+// const DropdownItem = (props) => {
+//   return (
+//     <li className='dropdown-item'>
+//       <a href={props.url}>
+//         {props.title}
+//       </a>
+//     </li>
+//   );
+// }
 
-export default DropdownItem;
+// export default DropdownItem;
