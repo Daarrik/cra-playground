462bf18781799aedeca3fa3bf7d96753db9c671e
Daarrik <darrikwh@gmail.com>
2022-03-30 23:45:33 -0700

    dan abramov mind numbing solution for interval

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(count + 1);
+  }, 1000);
+
+  return <div>{count}</div>;
 };
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      console.log("inside tick");
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      console.log("inside if");
+      console.log(delay);
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 export default Products;
